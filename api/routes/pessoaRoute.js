f320f6648d6c9146a605775a88b56b73dfbe3927
nat-1501 <natalisoares90@gmail.com>
2022-04-26 10:50:44 -0300

    utilizando metodo CRUD

--- api/routes/pessoaRoute.js
+++ api/routes/pessoaRoute.js
@@ -4,5 +4,8 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas) 
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa) 
+router.post('/criarpessoas', PessoaController.criaPessoa) 
+
 
 module.exports = router
