0cb8505f13cc19d900f35b2ec6d84208b0a19ebb
nat-1501 <natalisoares90@gmail.com>
2022-04-26 23:36:44 -0300

    utilizando metodo update e delete

--- api/routes/pessoaRoute.js
+++ api/routes/pessoaRoute.js
@@ -6,6 +6,7 @@ const router = Router()
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas) 
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa) 
 router.post('/criarpessoas', PessoaController.criaPessoa) 
-
+router.put('/pessoas/:id', PessoaController.atualizaPessoa) 
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 
 module.exports = router
