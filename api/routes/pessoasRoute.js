f1097c67e5077351d1aa5568f7baf6768df2cff8
Nat-1501 <natalisoares90@gmail.com>
2022-05-04 17:14:15 -0300

    crud criado

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -9,5 +9,8 @@ router.post('/criarpessoas', PessoaController.criaPessoa)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa) 
 router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router
